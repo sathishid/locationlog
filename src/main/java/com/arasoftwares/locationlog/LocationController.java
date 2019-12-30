416630b2b27b9fd406781c2a6fb125f6abc12b8b
Sathish Babu <sathishbabur@hotmail.com>
2019-12-30 18:13:10 +0530

    Create post in controller

--- src/main/java/com/arasoftwares/locationlog/LocationController.java
+++ src/main/java/com/arasoftwares/locationlog/LocationController.java
@@ -3,9 +3,12 @@ package com.arasoftwares.locationlog;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,21 +18,25 @@ public class LocationController {
     @Autowired
     private LocationRepository locationRepository;
 
-    @GetMapping("")
-    public String postByUrl(@RequestParam(value = "lat") float lat, @RequestParam(value = "lng") float lng,
-            @RequestParam(value = "speed") float speed
-
-    ) {
-        Location location = new Location();
-        location.latitude = lat;
-        location.longitude = lng;
-        location.speed = speed;
-        locationRepository.save(location);
-        return "Success";
+    @PostMapping()
+    public Location postLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
     }
 
+    // @PostMapping(path = "", consumes = { "multipart/form-data" })
+    // public String postByUrl(@RequestParam("latitude") float latitude,
+    // @RequestParam("longitude") float longitude,
+    // @RequestParam("speed") float speed) {
+    // Location location = new Location();
+    // location.latitude = latitude;
+    // location.longitude = longitude;
+    // location.speed = speed;
+    // locationRepository.save(location);
+    // return "Success";
+    // }
+
     @GetMapping("all")
-    public Iterable<Location> getAll(){
+    public Iterable<Location> getAll() {
         return locationRepository.findAll();
     }
 
