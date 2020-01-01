a68f06e6e6382abdb8113a9f2907869c5c256b67
Sathish Babu <sathishbabur@hotmail.com>
2020-01-01 13:20:51 +0530

    Submitted via Get

--- src/main/java/com/arasoftwares/locationlog/LocationController.java
+++ src/main/java/com/arasoftwares/locationlog/LocationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +29,22 @@ public class LocationController {
     // @RequestParam("longitude") float longitude,
     // @RequestParam("speed") float speed) {
     // Location location = new Location();
-    // location.latitude = latitude;
+    // location.latitude = latitude;5
     // location.longitude = longitude;
     // location.speed = speed;
     // locationRepository.save(location);
     // return "Success";
     // }
+
+    @GetMapping("submit/{lat}")
+    public Location submit(@PathVariable("lat") float latitude) {
+        Location location = new Location();
+        location.latitude = latitude;
+        location.longitude = 1.4f;
+        location.speed = 2.4f;
+        return locationRepository.save(location);
+
+    }
 
     @GetMapping("all")
     public Iterable<Location> getAll() {
